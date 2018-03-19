dca41102acea2ec4237dfc47dcf2e8de3f1dc62f
James <jy1v07@gmail.com>
2018-03-19 00:07:29 +0000

    added db migrations initialcreate

---
+++ TransactionLedger/Models/PurchaseDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransactionLedger.Models
+{
+    public partial class PurchaseDetail
+    {
+        public int Id { get; set; }
+        public string ItemDescription { get; set; }
+        public int? ItemId { get; set; }
+        public int PurchaseId { get; set; }
+        public decimal Value { get; set; }
+
+        public Purchase Purchase { get; set; }
+    }
+}
---
+++ TransactionLedger/Models/SaleDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransactionLedger.Models
+{
+    public partial class SaleDetail
+    {
+        public int Id { get; set; }
+        public string ItemDescription { get; set; }
+        public int? ItemId { get; set; }
+        public int SaleId { get; set; }
+        public decimal Value { get; set; }
+
+        public Sale Item { get; set; }
+    }
+}
